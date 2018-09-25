47cdd10eb34cb66b478f9a480ffd58f46c2a5596
Isak Potgieter <isak.potgieter@gmail.com>
2018-09-25 09:37:35 +0200

    Clarify explanation in transactions.js

--- transactions.js
+++ transactions.js
@@ -4,7 +4,7 @@ We distribute prepaid debit cards that users can manage on our platform. Users c
 
 To find out what transactions were made using a card and to update the data on our side, we also need to use the external service. We are here only concerned with money spent with the card, not what was loaded onto it. The problem is, when we query a card, both successful and unsuccessful transactions are returned, without an indication of their status (this is actually happening, I didn't make it up). The only clue given by the bank is the total amount of all successful transactions with card. We have to fit the data to determine which transactions were successful. Every time we make an update, we take the latest status on our side as accurate and only consider new transactions.
 
-Below, "internal" represents the successful transactions as at time === 7. Now, time > 7 and "external1" and "external2" represent 2 possible scenarios encountered when updating the card transactions on our side. Notice that in each scenario the external data contains transactions that we've already discarded, because the collection of payments fitting the total amount of 434 at time === 7 didn't include them. The only relevant new transactions, therefore, are from time > 7.
+Below, "internal" represents the successful transactions up to time === 7. We need to update our data and "external1" and "external2" represent 2 possible scenarios encountered when updating the card transactions on our side. Notice that in each scenario there are transactions from time <= 7 that we've already excluded, because the amounts adding up to 434 at time === 7 are 80, 67, 252, 35. So, in scenario 1, we excluded 124 and 125 and in scenario 2 we excluded 212 and 256. The only relevant new transactions, therefore, are from time > 7.
 
 Write a function to determine which transactions fit the data and to update our internal copy accordingly. You can start with the simpler scenario 1, but the end result needs to be generic, so that it can work with either (or any other) case.
 
@@ -55,10 +55,10 @@ const external2 = {
     { time: 17, amount: 92 },
     { time: 22, amount: 255 },
     { time: 23, amount: 67 },
+    { time: 28, amount: 97 },
     { time: 28, amount: 50 },
-    { time: 28, amount: 110 },
-    { time: 39, amount: 97 },
-    { time: 41, amount: 253 },
+    { time: 39, amount: 253 },
+    { time: 41, amount: 110 },
     { time: 47, amount: 54 },
     { time: 49, amount: 146 },
     { time: 56, amount: 35 }
